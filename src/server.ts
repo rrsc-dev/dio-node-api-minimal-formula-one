e13b0f4a119b8c554e47d44b297105cbd95cef31
rrsc-dev <reis.rrsc@outlook.com>
2026-01-05 22:48:37 -0300

    array de dados

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,54 @@ import fastify from "fastify";
 
 const server = fastify({ logger: true });
 
+const teams = [
+    {
+        id: 1,
+        name: "Time 1",
+        base: "País A"
+    },
+    {
+        id: 2,
+        name: "Time 2",
+        base: "País B"
+    },
+    {
+        id: 3,
+        name: "Time 3",
+        base: "País C"
+    }
+];
+
+const drivers = [
+    {
+        id: 1,
+        name: "Um piloto 1",
+        team: "Time 1"
+    },
+    {
+        id: 2,
+        name: "Um piloto 2",
+        team: "Time 2"
+    },
+    {
+        id: 3,
+        name: "Um piloto 3",
+        team: "Time 3"
+    }
+];
+
 server.get("/teams", async ( request, response ) => {
     response.type("application/json").code(200);
-    return [{
-        id: 1,
-        name: "Time 1"
-    }]
+    return {
+        teams
+    }
 });
 
 server.get("/drivers", async ( request, response ) => {
     response.type("application/json").code(200);
-    return [{
-        id: 1,
-        name: "Um piloto",
-        team: "Time 1"
-    }]
+    return {
+        drivers
+    }
 });
 
 server.listen({ port: 3333 }, () => {
