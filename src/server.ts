8de4b4541a0ac3c5c0cbbc6dde4411d46e488174
rrsc-dev <reis.rrsc@outlook.com>
2026-01-05 22:05:21 -0300

    Métodos get

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+import fastify from "fastify";
+
+const server = fastify({ logger: true });
+
+server.get("/teams", async ( request, response ) => {
+    response.type("application/json").code(200);
+    return [{
+        id: 1,
+        name: "Time 1"
+    }]
+});
+
+server.get("/drivers", async ( request, response ) => {
+    response.type("application/json").code(200);
+    return [{
+        id: 1,
+        name: "Um piloto",
+        team: "Time 1"
+    }]
+});
+
+server.listen({ port: 3333 }, () => {
+    console.log("server init");
+});
